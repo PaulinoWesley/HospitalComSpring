1738458b29aab87195b62d28172549928e444fdb
paulinowesley <wesleypaulinogdl@gmail.com>
2022-02-22 22:46:44 -0300

    Controller de Consulta

--- src/main/java/br/com/hospital/wesley/controller/ConsultaController.java
+++ src/main/java/br/com/hospital/wesley/controller/ConsultaController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.hospital.wesley.dto.ConsultaFilterDto;
@@ -28,7 +27,7 @@ public class ConsultaController {
 
 	@GetMapping
 	public List<Consulta> findByFilter(ConsultaFilterDto filtro) {
-		return consultaRepository.findByFilter(filtro);
+		return consultaRepository.findByFilter(filtro);	
 	}
 
 	@GetMapping("/{id}")
